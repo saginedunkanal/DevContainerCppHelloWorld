9f37a693c6ed46d40079938a54da374c0f7f586a
Sagi Nedunkanal <saginedunkanal@users.noreply.github.com>
2022-12-17 14:40:49 +0100

    Add HelloWorld in C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "Hello, World!" << endl; // This prints Hello, World!
+   cout << "Hello, World2!" << endl; // This prints Hello, World!
+   cout << "Hello, World3!" << endl; // This prints Hello, World!
+   return 0;
+}
